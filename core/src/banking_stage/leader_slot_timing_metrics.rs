dd1de5aed125c38e4204377ddb3e3f5b12d5ade8
Rory Harris <rharri4@gmail.com>
2024-12-09 15:47:38 -0800

    Finished cleanup of leader slot timing metrics

--- core/src/banking_stage/leader_slot_timing_metrics.rs
+++ core/src/banking_stage/leader_slot_timing_metrics.rs
@@ -1,30 +1,30 @@
 use {
     solana_poh::poh_recorder::RecordTransactionsTimings,
-    solana_sdk::{clock::Slot, saturating_add_assign},
+    solana_sdk::clock::Slot,
     solana_timings::ExecuteTimings,
     std::{num::Saturating, time::Instant},
 };
 
 #[derive(Default, Debug)]
 pub struct LeaderExecuteAndCommitTimings {
-    pub collect_balances_us: u64,
-    pub load_execute_us: u64,
-    pub freeze_lock_us: u64,
-    pub record_us: u64,
-    pub commit_us: u64,
-    pub find_and_send_votes_us: u64,
+    pub collect_balances_us: Saturating<u64>,
+    pub load_execute_us: Saturating<u64>,
+    pub freeze_lock_us: Saturating<u64>,
+    pub record_us: Saturating<u64>,
+    pub commit_us: Saturating<u64>,
+    pub find_and_send_votes_us: Saturating<u64>,
     pub record_transactions_timings: RecordTransactionsTimings,
     pub execute_timings: ExecuteTimings,
 }
 
 impl LeaderExecuteAndCommitTimings {
     pub fn accumulate(&mut self, other: &LeaderExecuteAndCommitTimings) {
-        saturating_add_assign!(self.collect_balances_us, other.collect_balances_us);
-        saturating_add_assign!(self.load_execute_us, other.load_execute_us);
-        saturating_add_assign!(self.freeze_lock_us, other.freeze_lock_us);
-        saturating_add_assign!(self.record_us, other.record_us);
-        saturating_add_assign!(self.commit_us, other.commit_us);
-        saturating_add_assign!(self.find_and_send_votes_us, other.find_and_send_votes_us);
+        self.collect_balances_us += other.collect_balances_us;
+        self.load_execute_us += other.load_execute_us;
+        self.freeze_lock_us += other.freeze_lock_us;
+        self.record_us += other.record_us;
+        self.commit_us += other.commit_us;
+        self.find_and_send_votes_us += other.find_and_send_votes_us;
         self.record_transactions_timings
             .accumulate(&other.record_transactions_timings);
         self.execute_timings.accumulate(&other.execute_timings);
@@ -35,14 +35,14 @@ impl LeaderExecuteAndCommitTimings {
             "banking_stage-leader_slot_execute_and_commit_timings",
             "id" => id,
             ("slot", slot as i64, i64),
-            ("collect_balances_us", self.collect_balances_us as i64, i64),
-            ("load_execute_us", self.load_execute_us as i64, i64),
-            ("freeze_lock_us", self.freeze_lock_us as i64, i64),
-            ("record_us", self.record_us as i64, i64),
-            ("commit_us", self.commit_us as i64, i64),
+            ("collect_balances_us", self.collect_balances_us.0 as i64, i64),
+            ("load_execute_us", self.load_execute_us.0 as i64, i64),
+            ("freeze_lock_us", self.freeze_lock_us.0 as i64, i64),
+            ("record_us", self.record_us.0 as i64, i64),
+            ("commit_us", self.commit_us.0 as i64, i64),
             (
                 "find_and_send_votes_us",
-                self.find_and_send_votes_us as i64,
+                self.find_and_send_votes_us.0 as i64,
                 i64
             ),
         );
