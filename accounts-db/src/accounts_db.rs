5c4005c8dc637e2f128013808ad920c882fa0d52
Rory Harris <rory.harris@anza.xyz>
2025-03-14 23:23:01 +0000

    Reducing write

--- accounts-db/src/accounts_db.rs
+++ accounts-db/src/accounts_db.rs
@@ -141,11 +141,6 @@ const MAX_ITEMS_PER_CHUNK: Slot = 2_500;
 // This allows us to split up accounts index accesses across multiple threads.
 const SHRINK_COLLECT_CHUNK_SIZE: usize = 50;
 
-/// The number of shrink candidate slots that is small enough so that
-/// additional storages from ancient slots can be added to the
-/// candidates for shrinking.
-const SHRINK_INSERT_ANCIENT_THRESHOLD: usize = 10;
-
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq)]
 pub enum CreateAncientStorage {
     /// ancient storages are created by appending
@@ -4685,7 +4680,7 @@ impl AccountsDb {
             .store(shrink_candidates_slots.len() as u64, Ordering::Relaxed);
 
         let candidates_count = shrink_candidates_slots.len();
-        let ((mut shrink_slots, shrink_slots_next_batch), select_time_us) = measure_us!({
+        let ((shrink_slots, shrink_slots_next_batch), select_time_us) = measure_us!({
             if let AccountShrinkThreshold::TotalSpace { shrink_ratio } = self.shrink_ratio {
                 let (shrink_slots, shrink_slots_next_batch) =
                     self.select_candidates_by_total_usage(&shrink_candidates_slots, shrink_ratio);
@@ -4708,7 +4703,7 @@ impl AccountsDb {
 
         // If there are too few slots to shrink, add an ancient slot
         // for shrinking.
-        if shrink_slots.len() < SHRINK_INSERT_ANCIENT_THRESHOLD {
+        /*if shrink_slots.len() < SHRINK_INSERT_ANCIENT_THRESHOLD {
             let mut ancients = self.best_ancient_slots_to_shrink.write().unwrap();
             while let Some((slot, capacity)) = ancients.pop_front() {
                 if let Some(store) = self.storage.get_slot_storage_entry(slot) {
@@ -4728,7 +4723,7 @@ impl AccountsDb {
                     }
                 }
             }
-        }
+        }*/
         if shrink_slots.is_empty()
             && shrink_slots_next_batch
                 .as_ref()
